4c65f98ac51917e28d57075a104d5c07512d66de
Manu Gonzalez <jmgonzalez@ces.vocento.com>
2019-08-13 12:36:44 +0200

    Middleware message lifecycle logging chapter

--- src/Messenger/AuditMiddleware.php
+++ src/Messenger/AuditMiddleware.php
@@ -6,6 +6,8 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
+use Symfony\Component\Messenger\Stamp\ReceivedStamp;
+use Symfony\Component\Messenger\Stamp\SentStamp;
 
 class AuditMiddleware implements MiddlewareInterface
 {
@@ -29,6 +31,14 @@ class AuditMiddleware implements MiddlewareInterface
             'id' => $stamp->getUniqueId(),
             'class' => get_class($envelope->getMessage())
         ];
+
+        if ($envelope->last(ReceivedStamp::class)) {
+            $this->logger->info('[{id}] Received {class}', $context);
+        } elseif ($envelope->last(SentStamp::class)) {
+            $this->logger->info('[{id}] Sent {class}', $context);
+        } else {
+            $this->logger->info('[{id}] Handling sync {class}', $context);
+        }
 
         return $stack->next()->handle($envelope, $stack);
     }
